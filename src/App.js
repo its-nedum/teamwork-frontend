0e1d22eb78b29c23ee4daa931506956a7d931a0b
its-nedum <de.magnificent1@gmail.com>
2019-12-16 10:39:09 +0100

    chores(changed to yarn)

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import About from './components/links/about';
 // import Footer from './components/links/footer'
 import {createBrowserHistory} from 'history'
 export const history = createBrowserHistory()
-console.log(history)
+
 class App extends Component {
   render(){
     return (
